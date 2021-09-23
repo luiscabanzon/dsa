48a4906f11b7c75da246fd165772800ea8bfe611
luiscabanzon <luiscabanzon@gmail.com>
2021-09-24 00:08:08 +0200

    Update README, added pipelines

---
+++ pipelines/covid_survey_covid_mask.py
@@ -0,0 +1,97 @@
+from lab_utils import *
+
+import luigi
+import requests
+import json
+
+
+# ###################
+# AUXILIARY FUNCTIONS
+# ###################
+
+# Renders the table name for each different indicator
+def get_table_name(test_prefix):
+    return f'{test_prefix}covid_survey_covid_mask'
+
+
+# ##############
+# PIPELINE TASKS
+# ##############
+
+class CreateTable(luigi.Task):
+    """
+    Creates table in database.
+    """
+    test_prefix = luigi.Parameter(default='')
+    def run(self):
+        indicator_code = get_indicator_code('covid')
+        table_schema = (
+            ('percent_cli', 'float'),
+            ('cli_se', 'float'),
+            ('percent_cli_unw', 'float'),
+            ('cli_se_unw', 'float'),
+            ('cli_sample_size', 'NUMERIC'),
+            ('percent_mc', 'float'),
+            ('mc_se', 'float'),
+            ('percent_mc_unw', 'float'),
+            ('mc_se_unw', 'float'),
+            ('mc_sample_size', 'NUMERIC'),
+            ('country', 'text'),
+            ('iso_code', 'text'),
+            ('gid_0', 'text'),
+            ('survey_date', 'NUMERIC'),
+        )
+        create_table(get_table_name(self.test_prefix), table_schema)
+
+    def output(self):
+        return TableExists(get_table_name(self.test_prefix))
+
+
+class LoadTable(luigi.Task):
+    """
+    Loads report file into database.
+    """
+    test_prefix = luigi.Parameter(default='')
+    date = luigi.Parameter()
+
+    def get_sql_filter(self):
+        return f"survey_date = {self.date.replace('-','')}"
+
+    def requires(self):
+        return CreateTable(test_prefix=self.test_prefix)
+
+    def run(self):
+        # Detele data from the data we are inserting into (overwrite)
+        run_query(f"DELETE FROM {get_table_name(self.test_prefix)} WHERE {self.get_sql_filter()}")
+        # Insert data
+        run_query(f"""
+            INSERT INTO {get_table_name(self.test_prefix)}
+            SELECT 
+                percent_cli,
+                cli_se,
+                percent_cli_unw,
+                cli_se_unw,
+                a.sample_size AS cli_sample_size,
+                percent_mc,
+                mc_se,
+                percent_mc_unw,
+                mc_se_unw,
+                b.sample_size AS mc_sample_size,
+                a.country, 
+                a.iso_code,
+                a.gid_0, 
+                a.survey_date
+            FROM rpl_covid_survey_covid a
+            INNER JOIN rpl_covid_survey_mask b
+            ON a.survey_date = b.survey_date
+            AND a.iso_code = b.iso_code
+            WHERE
+                a.survey_date = {self.date.replace('-','')}
+        """)
+
+    def output(self):
+        return DataExists(table_name=get_table_name(self.test_prefix), where_clause=self.get_sql_filter())
+
+
+if __name__ == '__main__':
+     luigi.build([LoadTable()], workers=5, local_scheduler=True)
---
+++ pipelines/rpl_covid_survey.py
@@ -0,0 +1,113 @@
+from lab_utils import *
+
+import luigi
+import requests
+import json
+
+
+# ###################
+# AUXILIARY FUNCTIONS
+# ###################
+
+# Renders the file path for the report to download
+def get_file_path(indicator, country, date):
+    date_no_dash = date.replace('-', '')
+    return f'data/raw/covid_survey__{indicator}__{country}__{date_no_dash}__{date_no_dash}.txt'
+
+# Renders the table name for each different indicator
+def get_table_name(indicator, test_prefix):
+    return f'{test_prefix}{COVID_SURVEY_TABLE_NAME}_{indicator}'
+
+
+# ##############
+# PIPELINE TASKS
+# ##############
+
+class CreateTable(luigi.Task):
+    """
+    Creates table in database.
+    """
+    test_prefix = luigi.Parameter(default='')
+    indicator = luigi.Parameter()
+
+    def run(self):
+        indicator_code = get_indicator_code(self.indicator)
+        table_schema = (
+            (f'percent_{indicator_code}', 'float'),
+            (f'{indicator_code}_se', 'float'),
+            (f'percent_{indicator_code}_unw', 'float'),
+            (f'{indicator_code}_se_unw', 'float'),
+            ('sample_size', 'NUMERIC'),
+            ('country', 'text'),
+            ('iso_code', 'text'),
+            ('gid_0', 'text'),
+            ('survey_date', 'NUMERIC'),
+        )
+        create_table(get_table_name(self.indicator, self.test_prefix), table_schema)
+
+    def output(self):
+        return TableExists(get_table_name(self.indicator, self.test_prefix))
+
+
+class DownloadAPIReport(luigi.Task):
+    """
+    Downloads report from API.
+    """
+    indicator = luigi.Parameter()
+    country = luigi.Parameter()
+    date = luigi.Parameter()
+
+    def run(self):
+        date_no_dash = self.date.replace('-', '')
+        file_path = get_file_path(self.indicator, self.country, self.date)
+        url = f"https://covidmap.umd.edu/api/resources?indicator={self.indicator}&type=daily&country={self.country}&daterange={date_no_dash}-{date_no_dash}"
+        print(url)
+        response = requests.get(url).text
+        response_dict = json.loads(response)
+        df = pd.DataFrame.from_dict(response_dict['data'])
+        df.to_csv(file_path, sep='\t', index=False, encoding='utf-8')
+
+    def output(self):
+        file_path = get_file_path(self.indicator, self.country, self.date)
+        return luigi.LocalTarget(file_path)
+        
+
+class LoadReportIntoDB(luigi.Task):
+    """
+    Loads report file into database.
+    """
+    test_prefix = luigi.Parameter(default='')
+    indicator = luigi.Parameter()
+    country = luigi.Parameter()
+    date = luigi.Parameter()
+
+    def get_sql_filter(self):
+        return f"country='{self.country}' AND survey_date = {self.date.replace('-','')}"
+
+    def requires(self):
+        yield DownloadAPIReport(indicator=self.indicator, country=self.country, date=self.date)
+        yield CreateTable(indicator=self.indicator, test_prefix=self.test_prefix)
+
+    def run(self):
+        file_path = get_file_path(self.indicator, self.country, self.date)
+        load_file_in_table(file_path, get_table_name(self.indicator, self.test_prefix), overwrite_filter=self.get_sql_filter())
+
+    def output(self):
+        return DataExists(table_name=get_table_name(self.indicator, self.test_prefix), where_clause=self.get_sql_filter())
+
+
+class MasterTask(luigi.WrapperTask):
+    """
+    Generates tasks for several survey indicators and countries.
+    """
+    test_prefix = luigi.Parameter(default='')
+    date = luigi.Parameter()
+
+    def requires(self):
+        for indicator in ['mask', 'finance']:
+            for country in ['Germany', 'Japan']:
+                yield LoadReportIntoDB(indicator=indicator, country=country, date=self.date, test_prefix=self.test_prefix)
+
+
+if __name__ == '__main__':
+     luigi.build([MasterTask()], workers=5, local_scheduler=True)
